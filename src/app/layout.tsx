37b68ff2014a9d86a6645f294868dbe9b3f7acef
Kaleu B Farias <kaleufarias96@gmail.com>
2024-12-13 12:06:01 -0300

    ✨ Atualiza o layout da aplicação para incluir um botão de monitoramento e uma imagem do mapa; altera o idioma para pt-BR e ajusta a estrutura do corpo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/ui/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "SIAPESQ",
@@ -16,15 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        
-      >
+    <html lang="pt-BR">
+      <body className="h-screen flex flex-col">
         <SidebarProvider>
           <SidebarTrigger />
+          <main className="flex-1 flex flex-col items-center justify-center gap-4">
+            <img src="/mapa.png" alt="Mapa" />
+            <Link href="/#">
+              <Button className="bg-lightblue font-bold hover:bg-darkblue h-12 w-72">
+                Iniciar Monitoramento
+              </Button>
+            </Link>
+          </main>
           <AppSidebar />
         </SidebarProvider>
-        
         {children}
       </body>
     </html>
